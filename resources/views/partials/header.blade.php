aeb06bab9581056584446337547179d139af6859
DamianoSpampinato <ghgg585@gmail.com>
2024-05-23 18:05:17 +0200

    feat add heaader

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div>dc comics</div>
+    <button><a href="{{route('comics.index')}}">vai ai comics</a></button>
+    <button><a href="{{route('comics.create')}}">Aggiungi comico</a></button>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [PageController::class, 'index'])->name('home');
 Route::resource('comics', ComicController::class);
